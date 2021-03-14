9c5fc214e6a10fa03ea89238ddaf13072cb26fd6
enemyseven <enemyseven@users.noreply.github.com>
2021-03-13 21:06:48 -0600

    Create watermark.py

---
+++ watermark.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+from PIL import Image, ImageDraw, ImageFont
+
+image = Image.new('RGBA', (1920, 1080), color = (255, 0, 0, 0))
+width, height = image.size
+
+draw = ImageDraw.Draw(image)
+text = "Mock Data Text"
+
+# Set Font parameters
+font = ImageFont.truetype('HelveticaNeue.ttc', 82)
+textwidth, textheight = draw.textsize(text, font)
+
+margin = 10
+x = width - textwidth - margin
+y = height - textheight - margin
+
+# Finally draw the watermark in the bttom right corner
+draw.text((x,y), text, font=font, fill=(220,220,220,255))
+# Open temporary file
+image.show()
+
+# Show mode info
+print("Mode: ", image.mode)
+
+# Save watermarked Image
+image.save('images/watermark.png')
