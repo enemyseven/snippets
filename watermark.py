31e14a5abd8605df9fe72077a68cfb719aae0575
enemyseven <enemyseven@users.noreply.github.com>
2021-04-02 13:40:45 -0500

    Update watermark.py
    Added a scaling factor in an attempt to make the font look better.

--- watermark.py
+++ watermark.py
@@ -13,21 +13,25 @@ def setup():
         os.makedirs("watermarks")
 
 def make_watermark(text, filepath):
-    image = Image.new('RGBA', (1920, 1080), color = (255, 0, 0, 0))
+    scale = 1
+    image = Image.new('RGBA', (1920 * scale, 1080 * scale), color = (255, 0, 0, 0))
     width, height = image.size
 
     draw = ImageDraw.Draw(image)
+    draw.fontmode = "L"
 
     # Set Font parameters
-    font = ImageFont.truetype('HelveticaNeue.ttc', 108, 1)
+    font = ImageFont.truetype('HelveticaNeue.ttc', 108 * scale, 1)
     textwidth, textheight = draw.textsize(text, font)
 
     margin = 0
     x = (width - textwidth) / 2 # - Centered
-    y = (height - textheight) / 2 - 339 # - Centered
+    y = (height - textheight) / 2 - (339 * scale) # - Centered
 
     # Finally draw the watermark in the bttom right corner
     draw.text((x,y), text, font=font, fill=(12,32,116,255))
+    
+    img_resized = image.resize((1280,720), Image.LANCZOS)
     
     # only do this if running as a script
     if __name__ == "__main__":
@@ -38,7 +42,7 @@ def make_watermark(text, filepath):
         print("Mode: ", image.mode)
 
     # Save watermarked Image
-    image.save(filepath)
+    img_resized.save(filepath)
 
 def main():
     setup()
