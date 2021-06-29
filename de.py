fa2fd7ba518fd3be625606d3663fb0c34867fe10
enemyseven <enemyseven@users.noreply.github.com>
2021-06-28 23:06:38 -0500

    Update de.py
    Moved a large section of networking code into a function.

--- de.py
+++ de.py
@@ -13,6 +13,39 @@ import glob
 import urllib.request
 import urllib.error
 
+def downloadFile(sourceURL, destinationURL):
+    try:
+        resp = urllib.request.urlopen(sourceURL)
+    except urllib.error.HTTPError as e:
+        if hasattr(e, 'reason'):
+            logging.error("Error: Failed to reach the server.\n\t\t\t\t\tRef: " + sourceURL)
+            print('\t\tFailed to reach the server.')
+            print('\t\tReason: ', e.reason)
+        elif hasattr(e, 'code'):
+            logging.error("Error: Server couldn't fulfill the request.\n\t\t\t\t\tRef: " + sourceURL)
+            print('\t\tThe server couldn\'t fulfill the request.')
+            print('\t\tError code: ', e.code)
+        return False
+    except urllib.error.URLError as e:
+        logging.error("Error: " + e.reason + "\n\t\t\tRef: " + sourceURL)
+        print("\t\tURLError\n\t\t" + e.reason)
+        return False
+    except TimeoutError as e:
+        logging.error("Error: TimeoutError\n\t\t\tRef: " + url)
+        print("\t\tTimeoutError\n\t\t")
+    else:
+        if resp.getcode() != 200:
+            logging.error("Error: Not found on server.\n\t\t\t\tRef: " + sourceURL)
+            print("\t\tError: File not found on server.")
+            return False
+        else:
+            with open( destinationURL, "wb") as newfile:
+                newfile.write(resp.read())
+                logging.info(destinationURL + ": OK.")
+                print("\t\tSuccessfully downloaded.")
+                return True
+
+
 # Setting script call time
 callTime = datetime.datetime.today()
 
@@ -84,36 +117,10 @@ for url in urls:
         logging.warning("Warning: " + filename + " already Exists.")
         filesSkipped += 1
     else:
-        try:
-            resp = urllib.request.urlopen(url)
-        except urllib.error.HTTPError as e:
-            if hasattr(e, 'reason'):
-                logging.error("Error: Failed to reach the server for " + filename)
-                print('\t\tFailed to reach the server.')
-                print('\t\tReason: ', e.reason)
-            elif hasattr(e, 'code'):
-                logging.error("Error: Server couldn't fulfill the request for "+ filename)
-                print('\t\tThe server couldn\'t fulfill the request.')
-                print('\t\tError code: ', e.code)
-            filesNotFound += 1
-        except urllib.error.URLError as e:
-            logging.error("Error: " + e.reason + "\n\t\t\t\t\tRef: " + url)
-            print("\t\tURLError: Perhaps server does not exist.")
-        except TimeoutError as e:
-            logging.error("Error: TimeoutError\n\t\t\tRef: " + url)
-            print("\t\tTimeoutError\n\t\t")
+        if (decommon.downloadFile(url, category + '/' + filename)):
+            filesWritten += 1
         else:
-            if resp.getcode() != 200:
-                logging.error("Error: " + filename + " Not found on server.")
-                print("\t\tError: " + filename + " not found on server.")
-                filesNotFound += 1
-                errorOccured = True
-            else:
-                with open( category + "/" + filename, "wb") as newfile:
-                    newfile.write(resp.read())
-                    logging.info(filename + " OK.")
-                    print("\t\tStatus: Success")
-                    filesWritten += 1
+            filesNotFound += 1
 
 print("\n\n\t---- Finished ----")
 print("\tWritten:\t" + str(filesWritten))
